873ec865fd204a15f51d912e21e38660dd211aaa
Nya-Nya-Hoshino <Nya-Nya-Hoshino@users.noreply.github.com>
2026-06-05 15:58:34 +0800

    fix: 移除无用导入 PlatformAdapterType 和 asyncio

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import random
-import asyncio
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star
 from astrbot.api import logger
-from astrbot.api.platform import PlatformAdapterType
 
 
 WOOD_SOUNDS = [
